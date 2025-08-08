ae3d8328af0d7c989e6212303aea0e0d11219300
Yunuscan-Cemil-Yilmaz <yilmazyunuscancemil00@gmail.com>
2025-08-09 01:58:20 +0300

    add error pages components

---
+++ client/src/app/shared/error-pages/not-found-error/not-found-error.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-not-found-error',
+  imports: [],
+  templateUrl: './not-found-error.html',
+  styleUrl: './not-found-error.scss'
+})
+export class NotFoundError {
+
+}
