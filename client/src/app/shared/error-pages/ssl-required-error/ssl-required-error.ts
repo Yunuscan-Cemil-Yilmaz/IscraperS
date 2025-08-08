b07d30be991e9417477cb60e4a9ee030f345c5fc
Yunuscan-Cemil-Yilmaz <yilmazyunuscancemil00@gmail.com>
2025-08-09 01:58:35 +0300

    add ssl error page component

---
+++ client/src/app/shared/error-pages/ssl-required-error/ssl-required-error.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ssl-required-error',
+  imports: [],
+  templateUrl: './ssl-required-error.html',
+  styleUrl: './ssl-required-error.scss'
+})
+export class SslRequiredError {
+
+}
