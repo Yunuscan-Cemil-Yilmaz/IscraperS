5f653a79716f1256f519fe11cbe18a8e1e792ab4
Yunuscan-Cemil-Yilmaz <yilmazyunuscancemil00@gmail.com>
2025-08-09 00:10:27 +0300

    add app.config.ts file and create layout component

---
+++ client/src/app/core/layout/application-layout/application-layout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-application-layout',
+  imports: [],
+  templateUrl: './application-layout.html',
+  styleUrl: './application-layout.scss'
+})
+export class ApplicationLayout {
+
+}
