eb3e65423b3d29002e437bba68fa4e1a00750851
Yunuscan-Cemil-Yilmaz <yilmazyunuscancemil00@gmail.com>
2025-08-09 11:41:39 +0300

    add home, login, and register components with templates and styles

--- client/src/app/core/config/app.config.ts
+++ client/src/app/core/config/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from "@angular/router";
+import { PreloadAllModules, provideRouter, withInMemoryScrolling, withPreloading } from "@angular/router";
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { routes } from '../../app.routes';
@@ -9,7 +9,11 @@ export const appConfig: ApplicationConfig = {
     providers: [
         provideBrowserGlobalErrorListeners(),
         provideZonelessChangeDetection(),
-        provideRouter(routes),
+        provideRouter(
+            routes,
+            withPreloading(PreloadAllModules),
+            withInMemoryScrolling({ scrollPositionRestoration: 'enabled' })
+        ),
         provideHttpClient()
     ]
 };
