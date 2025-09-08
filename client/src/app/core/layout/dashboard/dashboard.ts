39cbb654b949200d8e2b5a43ee4bd22bd131cca2
Yunuscan-Cemil-Yilmaz <yilmazyunuscancemil00@gmail.com>
2025-09-08 20:57:01 +0300

    styled dashboard layout

--- client/src/app/core/layout/dashboard/dashboard.ts
+++ client/src/app/core/layout/dashboard/dashboard.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from "@angular/router";
 import { Topbar } from "../topbar/topbar";
+import { Sidebar } from "../sidebar/sidebar";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [RouterOutlet, Topbar],
+  imports: [RouterOutlet, Topbar, Sidebar],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss'
 })
