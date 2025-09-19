41ab9ba7c7f2728f978c1f725b2184cfffa866cd
Yunuscan-Cemil-Yilmaz <yilmazyunuscancemil00@gmail.com>
2025-09-19 23:16:25 +0300

    add profile navbar component design

--- client/src/app/features/pages/profile/profile-navbar/profile-navbar.ts
+++ client/src/app/features/pages/profile/profile-navbar/profile-navbar.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject, DestroyRef } from '@angular/core';
+import { RouterLink } from "@angular/router";
+import { Router, NavigationEnd } from '@angular/router';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-profile-navbar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './profile-navbar.html',
   styleUrl: './profile-navbar.scss'
 })
 export class ProfileNavbar {
+  private router = inject(Router)
+  private destroyRef = inject(DestroyRef);
 
+  public isInformation: boolean = false;
+  public isScrapers: boolean = false;
+  public isSettings: boolean = false;
+
+  constructor () { 
+    this.router.events
+      .pipe(
+        filter((e): e is NavigationEnd => e instanceof NavigationEnd),
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe((event: NavigationEnd) => {
+        this.isInformation = false; this.isScrapers = false; this.isSettings = false;
+        const url: string = event.urlAfterRedirects;
+        if (url === '/profile') this.isInformation = true;
+        else if (url === '/profile/my-scrapers') this.isScrapers = true;
+        else if (url === '/profile/profile-settings') this.isSettings = true;
+      });
+  }
 }
